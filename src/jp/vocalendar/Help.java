2ea3053c8c68435765c74f080dac13e81b9f9183
nat0468 <nat@plala.to>
2013-02-09 01:28:42 +0900

    ベータリリース版

---
+++ src/jp/vocalendar/Help.java
@@ -0,0 +1,28 @@
+package jp.vocalendar;
+
+import java.util.Locale;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+/**
+ * ヘルプを開くクラス
+ */
+public class Help {
+	private Help() { } // インスタンス生成禁止
+	
+	private static final String HELP_URL =
+			"http://vocalendar.jp/apps/android/help/help.html";
+	
+	public static void openHelp(Context context) {
+		Log.d("Heko", "openHelp() " + HELP_URL);		
+		Intent i = new Intent();
+		i.setAction(Intent.ACTION_VIEW);
+		i.setData(Uri.parse(HELP_URL));
+		context.startActivity(i);
+	}
+	
+	
+}
