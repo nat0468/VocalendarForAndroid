c33e5dae803d013496518c741dd712efb5c59b6f
nat0468 <nat@plala.to>
2014-01-02 16:57:03 +0900

    通知機能まわりのソース追加

---
+++ src/jp/vocalendar/receiver/AlarmReceiver.java
@@ -0,0 +1,152 @@
+package jp.vocalendar.receiver;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.TimeZone;
+
+import jp.vocalendar.R;
+import jp.vocalendar.activity.EventListActivity;
+import jp.vocalendar.model.EventDataBase;
+import jp.vocalendar.model.EventDataBaseRow;
+import jp.vocalendar.model.FavoriteEventDataBaseRow;
+import jp.vocalendar.util.DateUtil;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat.Builder;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * 毎日のイベント通知を行うタイミングでのAlarmを受け取るReceiver
+ *
+ */
+public class AlarmReceiver extends BroadcastReceiver {
+	private static String TAG = "AlarmReceiver";
+	
+	
+	public static int REQUEST_CODE = 100;
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.d(TAG, "onReceive");
+		EventDataBaseRow[] rows = getNotificationEvents(context);
+		makeNotification(context, rows);
+		Log.d(TAG, "onReceive finished.");
+	}
+	
+	private void makeNotification(Context context, EventDataBaseRow[] rows) {
+        Intent intent = new Intent(context, EventListActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+        		context, EventListActivity.REQUEST_CODE_OPEN_TODAY, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(),
+                R.drawable.ic_vocalendar);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder.setContentTitle(makeTitle(context, rows));
+        builder.setContentText(makeContent(context, rows));        		
+        builder.setSmallIcon(R.drawable.ic_small_vocalendar);
+        builder.setLargeIcon(largeIcon);
+        builder.setTicker(makeTicker(context, rows));
+        builder.setAutoCancel(true);
+        builder.setDefaults(Notification.DEFAULT_ALL);
+        builder.setContentIntent(pendingIntent);
+        buildInBoxStyle(builder, context, rows);
+        
+        NotificationManager manager =
+        		(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.cancelAll(); // 前の通知があればいったん消す。
+        manager.notify(0, builder.build());
+	}
+
+	/**
+	 * Android 4.1以降のためのBig text styleの通知を組み立てる。
+	 * @param builder
+	 * @param context
+	 * @param rows
+	 */
+	private void buildInBoxStyle(
+			Builder builder, Context context, EventDataBaseRow[] rows) {
+		NotificationCompat.InboxStyle inboxStyle =
+		        new NotificationCompat.InboxStyle();
+		for(EventDataBaseRow row : rows) {			
+		    inboxStyle.addLine(row.getEvent().getSummary());
+			Log.d(TAG, "buildBigTextStyle:addLine() " + row.getEvent().getSummary());
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(context.getString(R.string.favorite));
+		sb.append(rows.length);
+		sb.append(context.getString(R.string.unit_of_events));				
+		inboxStyle.setSummaryText(sb.toString());
+		
+		builder.setStyle(inboxStyle);		
+	}
+
+	protected String makeTitle(Context context, EventDataBaseRow[] rows) {
+		StringBuilder title = new StringBuilder();
+        title.append(context.getString(R.string.todays_events_notifications));
+        title.append(context.getString(R.string.column));
+        title.append(rows.length);
+        title.append(context.getString(R.string.unit_of_events));
+		return title.toString();
+	}
+
+	protected String makeTicker(Context context, EventDataBaseRow[] rows) {
+		StringBuilder sb = new StringBuilder();
+        String comma = context.getString(R.string.comma);
+        boolean addComma = false;
+        for(EventDataBaseRow row : rows) {
+        	if(addComma) { sb.append(comma); }
+        	sb.append(row.getEvent().getSummary());
+        	addComma = true;
+        }
+        String message = sb.toString();
+		return message;
+	}
+
+	protected String makeContent(Context context, EventDataBaseRow[] rows) {
+		StringBuilder sb = new StringBuilder();
+		if(rows.length == 0) {
+			sb.append(context.getString(R.string.no_event));
+			return sb.toString();
+		}
+		sb.append(rows[0].getEvent().getSummary());
+		if(rows.length > 1) {
+			sb.append(" ");
+			sb.append(context.getString(R.string.etc));
+		}
+		return sb.toString();
+	}
+
+	
+	private EventDataBaseRow[] getNotificationEvents(Context context) {
+		EventDataBase db = new EventDataBase(context);
+		db.open();
+		TimeZone timeZone = TimeZone.getDefault();
+		Calendar today = Calendar.getInstance(timeZone);
+		Date todayDate = today.getTime();
+		List<FavoriteEventDataBaseRow> rows = 
+				new LinkedList<FavoriteEventDataBaseRow>();
+		db.getAllFavoriteEvents(today, timeZone, rows);
+		db.close();
+		
+		List<FavoriteEventDataBaseRow> todayRows = new LinkedList<FavoriteEventDataBaseRow>();
+		for(FavoriteEventDataBaseRow row : rows) {
+			if(row.getEvent().equalByDate(todayDate, timeZone)) {
+				todayRows.add(row);
+			}
+		}
+		
+		return todayRows.toArray(new EventDataBaseRow[todayRows.size()]);
+	}
+}
---
+++ src/jp/vocalendar/receiver/AlarmReceiverSetter.java
@@ -0,0 +1,106 @@
+package jp.vocalendar.receiver;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import jp.vocalendar.Constants;
+import jp.vocalendar.R;
+import jp.vocalendar.VocalendarApplication;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * イベントを通知するためのAlarmReceiverを設定する処理を実装したクラス。
+ * AlarmReceiverを設定するべきイベント(OS起動、日付変更など)が発生したときに
+ * AlarmReceiverを設定するReceiverでもある。
+ */
+public class AlarmReceiverSetter extends BroadcastReceiver {
+	private static String TAG = "AlarmReceiverSetter";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.d(TAG, "onReceive");
+		int notificationTime = VocalendarApplication.getNotificationTime(context);
+	    setAlarmReceiverToAlarmManager(context, notificationTime);
+		Log.d(TAG, "onReceive finished.");
+	}
+	
+	public static void setAlarmReceiverToAlarmManager(Context context, int notificationTime) {
+		Log.d(TAG, "setAlarmReceiverToAlarmManager");
+		
+		if(notificationTime == Constants.NOT_NOTIFY_PREFERENCE_VALUE) {
+			Log.d(TAG, "not notify event");
+			return;
+		}
+		
+		Intent intent = new Intent(context, AlarmReceiver.class);
+
+		PendingIntent sender = PendingIntent.getBroadcast(
+				context, AlarmReceiver.REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+		// 通知時間を設定
+		Calendar calSet = Calendar.getInstance();
+		calSet.setTimeInMillis(System.currentTimeMillis());
+		calSet.setTimeZone(TimeZone.getDefault());
+		calSet.set(Calendar.HOUR_OF_DAY, notificationTime);
+		calSet.set(Calendar.MINUTE, 0);
+		calSet.set(Calendar.SECOND, 0);
+		calSet.set(Calendar.MILLISECOND, 0);
+		if(calSet.getTimeInMillis() < System.currentTimeMillis()) {
+			// 過去の日時になった場合は、次の日にする
+			calSet.add(Calendar.DATE, 1);
+		}
+
+		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+		alarmManager.setInexactRepeating(
+				AlarmManager.RTC_WAKEUP, calSet.getTimeInMillis(), AlarmManager.INTERVAL_DAY, sender);
+
+		Log.d(TAG, "setInexactRepeating() finished.");		
+	}
+	
+	/**
+	 * (テスト用)3秒後にイベント通知を実行する。
+	 * @param context
+	 */
+	public static void setAlarmReceiverToAlarmManagerSoon(Context context) {
+		Log.d(TAG, "setAlarmReceiverToAlarmManager");		
+		Intent intent = new Intent(context, AlarmReceiver.class);
+
+		PendingIntent sender = PendingIntent.getBroadcast(
+				context, AlarmReceiver.REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+		// 3秒後に通知を表示させる
+		Calendar calSet = Calendar.getInstance();
+		calSet.setTimeInMillis(System.currentTimeMillis());
+		calSet.setTimeZone(TimeZone.getDefault());
+		calSet.add(Calendar.SECOND, 3);
+
+		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+		alarmManager.setRepeating(
+				AlarmManager.RTC_WAKEUP, calSet.getTimeInMillis(), AlarmManager.INTERVAL_DAY, sender);
+		Log.d(TAG, "setInexactRepeating() finished.");		
+	}
+
+	/**
+	 * 通知時間の値に対応する文字列を返す。
+	 * @param entryValue
+	 * @param context
+	 * @return
+	 */
+	public static String toEntry(String entryValue, Context context) {
+		String[] entryValues =
+				context.getResources().getStringArray(R.array.notification_time_entry_values);
+		for(int i = 0; i < entryValues.length; i++) {
+			if(entryValues[i].equals(entryValue)) {
+				return context.getResources().getStringArray(R.array.notification_time_entries)[i];
+			}
+		}
+		return null;
+	}
+
+}
